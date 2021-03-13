a3a2e0d7a188ea368dfbd1d90546f6751c16363d
Ce Pang <pcdota123@gmail.com>
2021-03-12 21:14:21 -0800

    Custom Circle Class

--- src/CirclePanel.java
+++ src/CirclePanel.java
@@ -35,7 +35,7 @@ public class CirclePanel extends JPanel {
 		g.setFont(sanSerifFont);
 	
 		fm = g.getFontMetrics();
-		g.drawString(text, 40, 52);	
+		g.drawString(text, 40, 50);	
 	}
 	
 	public void setText(String text) {
